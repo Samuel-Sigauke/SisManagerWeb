266d58404016ced0f93aa5d0e1569d8ed8835192
unknown <samuel@health263.systems>
2020-07-30 09:20:32 +0200

    login files added

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  onLogin() {
+    if(this.email &&  this.password){
+      this.router.navigate(['student/list'])
+    }
+  }
+
+}
